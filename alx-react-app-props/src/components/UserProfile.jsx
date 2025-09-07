120aa06ff2880472c72f85efb7da0da246b9aa6a
Malika Edaif <edmalika957@gmail.com>
2025-09-07 20:57:51 +0100

    create context

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,9 +1,17 @@
-const UserProfile = (props) => {
+
+
+import { useContext } from "react";
+
+
+
+function UserProfile() {
+
+const username = useContext(UserContext)
    return (
      <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
+       <h2>{username.name}</h2>
+       <p>Age: {username.age}</p>
+       <p>Bio: {username.bio}</p>
      </div>
    );
  };
