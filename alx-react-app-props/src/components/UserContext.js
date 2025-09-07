fd2829ce5baca5cb4a87e7c7c9b697e2d9a76870
Malika Edaif <edmalika957@gmail.com>
2025-09-07 20:42:22 +0100

    create usecontexte

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -3,4 +3,9 @@ import { createContext } from "react";
 export const context = createContext();
 
 
+function usercontext() {
+  const data = useContext(context);
+  return <div>{data}</div>;
+}
 
+export default usercontext;
