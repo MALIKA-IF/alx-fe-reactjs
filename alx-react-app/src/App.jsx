3cef70034c5fa3d0633066ee557b0e1ef7e9d99f
Malika Edaif <edmalika957@gmail.com>
2025-09-07 18:58:45 +0100

    create file Counter.jsx and use useState

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -9,6 +9,7 @@ import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 
 
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -39,6 +40,7 @@ function App() {
        <MainContent />
        <Footer />
        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+       
     </>
   )
 }
