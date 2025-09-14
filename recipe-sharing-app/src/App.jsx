8d95968825dd6349022e2523b54425c161686995
Malika Edaif <edmalika957@gmail.com>
2025-09-14 19:59:04 +0100

    create project using zustand

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,8 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-import RecipeList from './Components'
-import { AddRecipeForm } from './Components/AddRecipeForm'
+import RecipeList from './components'
+import { AddRecipeForm } from './components'
 
 
 
